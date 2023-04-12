22d58e309059038cf381ccfd3e494c54ba5caccd
Amin Arriaga <aminlorenzo.14@gmail.com>
2023-04-12 09:32:31 -0400

    Copy images from paca-ui

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['paca-ui'],
-}
+  transpilePackages: ["paca-ui"],
+  webpack: (config, {}) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "./node_modules/paca-ui/src/stories/assets/images/"),
+            to: path.resolve(__dirname, "./public/images/"),
+          },
+        ],
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
