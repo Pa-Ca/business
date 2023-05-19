53040b0d753d02039565879819069e97822cbd98
JRBarreraM <mrjrbarreram@gmail.com>
2023-05-19 15:31:28 -0400

    (in progress) get reservations

--- pages/branch-reservations.tsx
+++ pages/branch-reservations.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { Box, BranchReserves } from "paca-ui";
 import useInputForm from "paca-ui/src/stories/hooks/useInputForm";
 import OptionObject from "paca-ui/src/stories/utils/objects/OptionObject";
+import { setBranches, setCurrentBranch } from "../src/context/slices/branches";
 import { MAIN_COLOR, SECONDARY_COLOR, GREEN } from "../src/config";
 import { useAppSelector } from "../src/context/store";
 import postReservationService from "../src/services/reservations/postReservationService";
@@ -14,6 +15,8 @@ import { setToken } from "../src/context/slices/auth";
 import ReservationDTO from "../src/objects/reservations/ReservationDTO";
 import { error } from "console";
 import logout from "../src/utils/logout";
+import generateValidHours from "../src/utils/generateValidHours";
+import validatePhone from "../src/utils/validatePhone";
 
 export default function BranchReservations() {
 
@@ -116,18 +119,18 @@ export default function BranchReservations() {
     }
 
     // Email validation
-    // const phoneValidation = validateEmail(phone.value);
-    // if (phoneValidation.code !== 0) {
-    //   valid = false;
-    //   phone.setError(true);
-    //   switch (phoneValidation.code) {
-    //     case 1:
-    //       phone.setErrorMessage("Formato de teléfono inválido.");
-    //       break;
-    //     default:
-    //       phone.setErrorMessage("Teléfono inválido.");
-    //   }
-    // }
+    const phoneValidation = validatePhone(phone.value);
+    if (phoneValidation.code !== 0) {
+      valid = false;
+      phone.setError(true);
+      switch (phoneValidation.code) {
+        case 1:
+          phone.setErrorMessage("Formato de teléfono inválido.");
+          break;
+        default:
+          phone.setErrorMessage("Teléfono inválido.");
+      }
+    }
 
     // Persons validation
     if (!persons.value || persons.value === ""){
@@ -172,233 +175,69 @@ export default function BranchReservations() {
   };
 
   const reservations = [
-    {
-      date: "12 de Febrero",
-      reservations: [
-        {
-          start: "6:00 PM",
-          owner: "Bernardo López",
-          ownerPhone: "0416-4813706",
-          persons: 4,
-          tables: 1,
-          state: 1,
-        },
-        {
-          start: "6:00 PM",
-          owner: "Juan Escudero",
-          ownerPhone: "0414-387911",
-          persons: 8,
-          tables: 2,
-          state: 1,
-        },
-        {
-          start: "7:00 PM",
-          owner: "Carlos Ribero",
-          ownerPhone: "0424-3050325",
-          persons: 10,
-          tables: 1,
-          state: 1,
-        },
-        {
-          start: "7:30 PM",
-          owner: "Daniel Sotillo",
-          ownerPhone: "0426-4897739",
-          persons: 12,
-          tables: 2,
-          state: 1,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Domingo Rastelli",
-          ownerPhone: "0412-5723079",
-          persons: 12,
-          tables: 3,
-          state: 1,
-        },
-      ],
-    },
-    {
-      date: "13 de Febrero",
-      reservations: [
-        {
-          start: "8:00 PM",
-          owner: "Emilio Duque",
-          ownerPhone: "0416-8352614",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Juliana Leon",
-          ownerPhone: "0426-8352314",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Rafael Rodriguez",
-          ownerPhone: "0412-9357317",
-          persons: 4,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "9:00 PM",
-          owner: "Elena Rodrigo",
-          ownerPhone: "0412-3357447",
-          persons: 6,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "9:30 PM",
-          owner: "Magdalena Alfaro",
-          ownerPhone: "0414-9357147",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-      ],
-    },
-    {
-      date: "14 de Febrero",
-      reservations: [
-        {
-          start: "7:00 PM",
-          owner: "Alejandro Moreno",
-          ownerPhone: "0426-9462614",
-          persons: 4,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "7:00 PM",
-          owner: "Fernando Zapatero",
-          ownerPhone: "0416-8352457",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:00 PM",
-          owner: "Elio Andrade",
-          ownerPhone: "0426-7351318",
-          persons: 7,
-          tables: 2,
-          state: 0,
-        },
-        {
-          start: "8:00 PM",
-          owner: "Oracio Duran",
-          ownerPhone: "0416-4354347",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Ligia Cabeza",
-          ownerPhone: "0412-5357187",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-      ],
-    },
-    {
-      date: "15 de Febrero",
-      reservations: [
-        {
-          start: "7:30 PM",
-          owner: "Ernesto Sivira",
-          ownerPhone: "0416-2432614",
-          persons: 4,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "7:30 PM",
-          owner: "Ignacio Cuadrado",
-          ownerPhone: "0426-5352457",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:00 PM",
-          owner: "Cristopher Bermudez",
-          ownerPhone: "0412-8351318",
-          persons: 7,
-          tables: 2,
-          state: 0,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Manuel Sucre",
-          ownerPhone: "0416-7354341",
-          persons: 4,
-          tables: 1,
-          state: 0,
-        },
-        {
-          start: "8:30 PM",
-          owner: "Andrea Dominguez",
-          ownerPhone: "0416-6357187",
-          persons: 2,
-          tables: 1,
-          state: 0,
-        },
-      ],
-    },
+    ...new Array(13).fill({
+      start: "6:00 PM",
+      date: "2021-10-10",
+      owner: "Ivan Tortolero",
+      ownerPhone: "0414-8732414",
+      persons: 6,
+      tables: 6,
+      state: 1,
+}),
+    ...new Array(5).fill({
+      start: "6:00 PM",
+      date: "2021-10-11",
+      owner: "Ivan Tortolero",
+      ownerPhone: "0414-8732414",
+      persons: 6,
+      tables: 6,
+      state: 0,
+    }),
+    ...new Array(8).fill({
+      start: "6:00 PM",
+      date: "2021-10-12",
+      owner: "Ivan Tortolero",
+      ownerPhone: "0414-8732414",
+      persons: 6,
+      tables: 6,
+      state: 0,
+    }),
+    ...new Array(12).fill({
+      start: "6:00 PM",
+      date: "2021-10-13",
+      owner: "Ivan Tortolero",
+      ownerPhone: "0414-8732414",
+      persons: 6,
+      tables: 6,
+      state: 1,
+    }),
   ];
-  const validHoursIn = [
-    { value: '09:00:00', name: '9:00 am' },
-    { value: '2', name: '9:30 am' },
-    { value: '3', name: '10:00 am' },
-    { value: '4', name: '10:30 am' },
-    { value: '5', name: '11:00 am' },
-    { value: '6', name: '11:30 am' },
-    { value: '7', name: '12:00 pm' },
-    { value: '8', name: '12:30 pm' },
-    { value: '9', name: '1:00 pm' },
-    { value: '10', name: '1:30 pm' },
-    { value: '11', name: '2:00 am' },
-    { value: '12', name: '2:30 pm' },
-    { value: '13', name: '3:00 pm' },
-    { value: '14', name: '3:30 pm' },
-    { value: '15', name: '4:00 pm' },
-    { value: '16', name: '4:30 pm' },
-    { value: '17', name: '5:00 pm' },
-  ];
+  
+  
+  const validHoursIn = generateValidHours(branch.hourIn,branch.hourOut);
+  const validHoursOut = validHoursIn.slice(1);
+  validHoursIn.pop();
 
-  const validHoursOut = [
-    { value: '1', name: '9:00 am' },
-    { value: '2', name: '9:30 am' },
-    { value: '3', name: '10:00 am' },
-    { value: '4', name: '10:30 am' },
-    { value: '5', name: '11:00 am' },
-    { value: '6', name: '11:30 am' },
-    { value: '7', name: '12:00 pm' },
-    { value: '8', name: '12:30 pm' },
-    { value: '9', name: '1:00 pm' },
-    { value: '10', name: '1:30 pm' },
-    { value: '11', name: '2:00 am' },
-    { value: '12', name: '2:30 pm' },
-    { value: '13', name: '3:00 pm' },
-    { value: '14', name: '3:30 pm' },
-    { value: '15', name: '4:00 pm' },
-    { value: '16', name: '4:30 pm' },
-    { value: '17', name: '5:00 pm' },
-  ];
+  const validHoursInForm = [{value: validHoursIn}, {name: validHoursIn}];
 
+  const userRole: "business" = "business";
   const header={
     logged: true,
     onPacaClick: () => {},
     onLogout: () => logout(auth.token!, auth.refresh!, dispatch, router, undefined, undefined, () => {}),
     onEditProfile: () => router.push("/profile"),
     onRightSectionClick: () => router.push("/branch-reservations"),
+    userRole: userRole,
+    currentBranch: !!branch ? `${branch.name!} | ${branch.location}` : "",
+    branchOptions: branches.map((branch, index) => {
+      return {
+        name: `${branch.name!} | ${branch.location}`,
+        func: () => {
+          dispatch(setCurrentBranch(index));
+          router.reload();
+        },
+      };
+    }),
     picture: "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?cs=srgb&dl=pexels-chan-walrus-941861.jpg&fm=jpg",
     name: "Sempre Dritto",
     color: "#EF7A08",
@@ -422,6 +261,7 @@ export default function BranchReservations() {
       phone={phone}
       email={email}
       haveBranch={branch !== undefined}
+      icon_size="450px"
       showModal={showModal}
       setShowModal={setshowModal}
       onSubmit={onSubmit}
