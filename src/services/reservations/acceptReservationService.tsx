c3f8e655eaf67ca1a5b17050e68b5160205dbca0
Carlos Sivira <carlos.csivira@gmail.com>
2023-05-19 16:02:56 -0400

    (add) accept, reject and cancel reservation services

---
+++ src/services/reservations/acceptReservationService.tsx
@@ -0,0 +1,31 @@
+import { API_ENDPOINT } from "../../config";
+import FetchResponse from "../../objects/FetchResponse";
+import ExceptionResponse from "../../objects/ExceptionResponse";
+
+/**
+ * @brief Accept reservation
+ *
+ * @param id reservation id
+ *
+ * @returns API response
+ */
+export default async (id: number): Promise<FetchResponse<null>> => {
+  const uri = `${API_ENDPOINT}/reservation/accept/${id}`;
+
+  try {
+    const response = await fetch(uri, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+
+    if (response.status === 200) {
+      return { isError: false };
+    } else {
+      const exception: ExceptionResponse = await response.json();
+      return { exception, isError: true };
+    }
+  } catch (e) {
+    return { error: e as Error, isError: true };
+  }
+};
