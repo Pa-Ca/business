4c6fa005416bd3cf517821820d86000b757474a2
Carlos Sivira <carlos.csivira@gmail.com>
2023-05-19 18:04:46 -0400

    (add) reservation services and organization

--- src/services/reservations/getReservationService.tsx
+++ src/services/reservations/getReservationService.tsx
@@ -1,30 +1,36 @@
 import { API_ENDPOINT } from "../../config";
+import ReservationDTO from "../../objects/branch/BranchDTO";
 import FetchResponse from "../../objects/FetchResponse";
 import ExceptionResponse from "../../objects/ExceptionResponse";
 
 /**
- * @brief Cancel reservation
+ * @brief Get the reservation given its id
  *
- * @param id reservation id
+ * @param id Reservation id
  *
  * @returns API response
  */
-export default async (id: number, token: string): Promise<FetchResponse<null>> => {
-  const uri = `${API_ENDPOINT}/reservation/cancel/${id}`;
+export default async (
+  id: number,
+  token: string,
+): Promise<FetchResponse<ReservationDTO>> => {
+  const uri = `${API_ENDPOINT}/reservation/${id}`;
 
   try {
     const response = await fetch(uri, {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
 
     if (response.status === 200) {
-      return { isError: false };
+      const data: ReservationDTO = await response.json();
+      return { data, isError: false };
     } else {
       const exception: ExceptionResponse = await response.json();
+      console.log(exception)
       return { exception, isError: true };
     }
   } catch (e) {
