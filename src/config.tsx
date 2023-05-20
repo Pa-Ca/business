43f54a0411a7e5fe31d71ee5687ef43ef43f9302
Carlos Sivira <carlos.csivira@gmail.com>
2023-05-20 19:22:51 -0400

    (fix) env vars fix

--- src/config.tsx
+++ src/config.tsx
@@ -1,23 +1,11 @@
 /**
  * Pa'ca backend endpoint
  */
-export const API_ENDPOINT = process.env.NEXT_PUBLIC_NEXTAUTH_URL;
-/**
- * Google client secret
- */
-export const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
+export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 /**
  * Google maps API key
  */
 export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-/**
- * Google client id
- */
-export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-/**
- * JWT secret token
- */
-export const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET;
 /**
  * App main color
  */
